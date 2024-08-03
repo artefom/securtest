03000865609dfa787a646527b8e4880705a8b0c3
Artyom Fomenko <artyom.fomenko@distribusion.com>
2024-08-04 02:17:21 +0300

    Remove server name

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ docker build -f .deploy/Dockerfile -t securtest .
 docker stop securtest || true
 docker rm securtest || true
 
-
-
-docker run -d -p 0.0.0.0:80:80 --name securtest securtest:latest
+docker run -d -p 0.0.0.0:80:80 --name securtest securtest
