8fbb2c8c343d6e656b69baf5553951ac36095572
Artyom Fomenko <artyom.fomenko@distribusion.com>
2024-08-04 02:08:40 +0300

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+docker build -f .deploy/Dockerfile -t securtest .
+
+docker stop securtest || true
+docker rm securtest || true
+
+docker run -d -p 80:80 --name securtest securtest:latest
